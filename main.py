c3641ea8a411c59ed311db327f858360d97b310a
Emin <makhmudovemin441@gmail.com>
2025-08-31 14:45:08 +0500

    db handling 1-45 dishes finished

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ bot = telebot.TeleBot('TOKEN')
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    from init_db import init_db
+    
+    init_db()
+    
     
     markup = types.InlineKeyboardMarkup()
     
@@ -24,27 +28,44 @@ def start(message):
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
     bot.answer_callback_query(callback.id)
+
     if callback.data in ['mcdonalds', 'kfc', 'burgerk', 'tanuki', 'starbucks']:
-        
+        from renaming_1 import rename
+        dt2 = rename(callback)
+
         markup = types.InlineKeyboardMarkup()
         back_btn = types.InlineKeyboardButton("⬅️ Назад", callback_data='back_1')
         markup.add(back_btn)
-        
-        from renaming_1 import rename
-        
-        dt2 = rename(callback)
-            
-        
-        bot.send_message(callback.message.chat.id, f"Введите название блюда из {dt2}", reply_markup=markup, parse_mode='html')
-        bot.delete_message(callback.message.chat.id, callback.message.id)
+
+        bot.edit_message_text(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            text=f"Введите название блюда из <b>{dt2}</b>",
+            parse_mode='HTML',
+            reply_markup=markup
+        )
+
     elif callback.data == 'back_1':
-        start(callback.message)
-    
-        bot.delete_message(callback.message.chat.id, callback.message.id)
-    
+        markup = types.InlineKeyboardMarkup()
+        mcdonald_btn = types.InlineKeyboardButton("McDonald’s", callback_data='mcdonalds')
+        kfc_btn = types.InlineKeyboardButton("KFC", callback_data='kfc')
+        burgerk_btn = types.InlineKeyboardButton("Burger King", callback_data='burgerk')
+        tanuki_btn = types.InlineKeyboardButton("Tanuki", callback_data='tanuki')
+        starbucks_btn = types.InlineKeyboardButton("Starbucks", callback_data='starbucks')
+        markup.add(mcdonald_btn, kfc_btn, burgerk_btn, tanuki_btn, starbucks_btn)
+
+        bot.edit_message_text(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            text='<b>Выберите ресторан из списка или введите название вручную</b>',
+            parse_mode='HTML',
+            reply_markup=markup
+        )
+
     else:
         bot.send_message(callback.message.chat.id, "Выберите ресторан!")
-        
+
+
     
 
 
